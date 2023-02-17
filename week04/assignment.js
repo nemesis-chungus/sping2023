a622063ce6767a4737357a00f07dcb77e5f5d418
nemesis-chungus <113367369+nemesis-chungus@users.noreply.github.com>
2023-02-17 10:15:49 -0500

    it works

--- week04/assignment.js
+++ week04/assignment.js
@@ -1,3 +1,4 @@
+console.log('Program Starting')
 const fs = require('fs');
 fs.readFile(__dirname + '\\assignment.txt', 'utf-8', function(error, data) {
     if (error) {
@@ -5,10 +6,12 @@ fs.readFile(__dirname + '\\assignment.txt', 'utf-8', function(error, data) {
     } else {
         var count = 0
         const lines = data.split("\n")
+        console.log('Reading A File')
         for (var line in lines) {
             const words = line.split(" ")
             count = count + words.length
         }
         console.log(`Words: ${count}`)
+        console.log('Done')
     }
 });
