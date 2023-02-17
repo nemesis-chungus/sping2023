9152a4773e42b4289e7e0baf194516a4d4c9b5cf
nemesis-chungus <113367369+nemesis-chungus@users.noreply.github.com>
2023-02-17 10:57:21 -0500

    new and improved

--- week04/assignment.js
+++ week04/assignment.js
@@ -2,16 +2,16 @@ console.log('Program Starting')
 const fs = require('fs');
 fs.readFile(__dirname + '\\assignment.txt', 'utf-8', function(error, data) {
     if (error) {
-        console.error(error);
-    } else {
-        var count = 0
-        const lines = data.split("\n")
-        console.log('Reading A File')
-        for (var line in lines) {
-            const words = line.split(" ")
-            count = count + words.length
-        }
-        console.log(`Words: ${count}`)
-        console.log('Done')
+    console.error(error);
+} else {
+    var count = 0
+    const lines = data.split("\n")
+    console.log('Reading A File')
+    for (var line in lines) {
+        const words = line.split(" ")
+        count = count + words.length
     }
+    console.log(`Words: ${count}`)
+    console.log('Done')
+}
 });
